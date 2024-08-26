147222b0161b8cfe0ba1fa34e82f0d1378f25eb8
tchaura <timchaura@gmail.com>
2024-08-26 18:44:58 +0300

    Added SQL function

---
+++ B1Task/CalculateSumAndMedian.sql
@@ -0,0 +1,27 @@
+-- FUNCTION: public.calculate_sum_and_median()
+
+-- DROP FUNCTION IF EXISTS public.calculate_sum_and_median();
+
+CREATE OR REPLACE FUNCTION public.calculate_sum_and_median(
+)
+    RETURNS TABLE(sum_of_integers bigint, median_of_decimals numeric)
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+
+BEGIN
+    SELECT SUM(evennumber) INTO sum_of_integers
+    FROM datatable;
+
+    SELECT PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY floatingnumber) INTO median_of_decimals
+    FROM datatable;
+
+    RETURN QUERY SELECT sum_of_integers, median_of_decimals;
+END;
+$BODY$;
+
+ALTER FUNCTION public.calculate_sum_and_median()
+    OWNER TO postgres;
